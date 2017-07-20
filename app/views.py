d317ffda5bfdcf97d1dcabb30f17b566ce2c7383
Clark Downer <cddowner@gmail.com>
2017-07-20 12:16:37 -0700

    Leverage existing model method

--- app/views.py
+++ app/views.py
@@ -1,11 +1,6 @@
 from flask import render_template, redirect, url_for
 from app import app, db, models
 from .utils import update_all_weather_reports
-# from datetime import datetime
-# import requests
-# from key import GOOGLE_API_KEY
-# from .parsers import parse_forecast_day, parse_forecast_date, parse_percent_precipitation
-# from sqlalchemy.orm.exc import MultipleResultsFound
 
 
 def log_error(message):
@@ -27,6 +22,6 @@ def get_new_reports():
 
 @app.route('/reports/')
 def display_reports():
-    reports = models.WeatherReport.query.all()
+    reports = models.get_all_weather_reports()
     return render_template('forecasts.html',
                            reports=reports)
